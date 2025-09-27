c6881ebdb8df0f4e5fc99d32cc3e10741f59f5f3
irrawaddy28 <xad890@gmail.com>
2025-09-27 04:50:38 -0700

    Update comments

--- Problem_3.py
+++ Problem_3.py
@@ -37,16 +37,19 @@ Generate all permutations, sort, locate the given permutation, return the next p
 Time: O(N^N), Space: O(N^N)
 
 2. Pivot, Swap, Reverse
-Step 1: Find the index of the pivot element (A[i] < A[i+1]).
+Step 1: Pivot
+Find the index of the pivot element (A[i] < A[i+1]).
 The element whose index i is such that the digit at index is smaller than the digit to the right. Thus, going backwards (right to left), the pivot element is the first element that is smaller than its successor. In other words, going from right to left, we search for the first digit which breaks the ascending order pattern. Eg.  123654, '3' is the pivot element.
 Exception: If the array is 54321, then there is no pivot element since the digits from right to left are arranged in ascending order. If no pivot element can be found, simply reverse the array,
 
-Step 2:  Find the index of the digit which lies to the right of the pivot and
-is the smallest digit greater than the pivot. We call this element SGP.
-We swap SGP with the pivot element Swap(SGP, pivot). Note: Elements to the right of pivot are in increasing order from right to left.
-Eg. 123654, 4 is SGP, 654 is in increasing order from right to left. After swapping with pivot, we get 124653
+Step 2: Supremum of Pivot to the right of pivot
+Find the index of the digit which lies to the right of the pivot and
+is the smallest digit greater than the pivot. We call this element SOP (supremum of pivot).
+We swap SOP with the pivot element Swap(SOP, pivot). Note: Elements to the right of pivot are in increasing order from right to left. Hence, to find
+the SOP, we scan the elements from right to left.
+Eg. In 123654, 3 is the pivot. Scanning elements from end to pivot (4->5->6), we find that 4 is the SOP. Swapping SOP with pivot, we get 124653
 
-Step 3: Reverse the digits from pivot+1 till the end
+Step 3: Reverse the digits from the right of pivot
 124653 -> 124356
 Thus, the next permutation of 123654 is 124356
 https://youtu.be/-1cLK6PaLsQ?t=975
@@ -73,17 +76,16 @@ def nextPermutation(nums: List[int]) -> None:
             pivot = i
             break
 
-    # Step 2: Find the index of the SGP (smallest digit greater than the
-    # pivot) to the right of pivot. Thus, find the first element in the
-    # increasing order that is > pivot
+    # Step 2: Find the index of the SOP to the right of pivot and swap pivot
+    # with SOP
     if pivot != -1:
         for i in range(N-1,pivot,-1):
             if nums[i] > nums[pivot]:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
                 break
 
-    # 3. Reverse all the elements starting from pivot+1 till the end of array
-    # reverse A[pivot + 1: end+1]
+    # Step 3. Reverse all the elements starting from pivot+1 till the end of
+    # array
     reverse(nums, pivot+1, N-1)
     return None
 
